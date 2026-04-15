f8d73ebfb12859cc7bd81b1058f25dae1a5f2dce
guiaiguaba <guiaiguaba@gmail.com>
2026-04-15 18:12:39 -0300

    type

--- backend/src/infra/storage/storage.service.ts
+++ backend/src/infra/storage/storage.service.ts
@@ -6,7 +6,7 @@ import {
   GetObjectCommand,
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { v4 as uuidv4 } from 'uuid';
+
 
 @Injectable()
 export class StorageService {
